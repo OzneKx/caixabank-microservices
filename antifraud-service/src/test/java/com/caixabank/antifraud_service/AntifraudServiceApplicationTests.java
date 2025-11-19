3a91f187deb149166e65ddb29be0daf62d8fed2d
OzneKx <kenzoalbuqk@gmail.com>
2025-11-18 22:37:40 -0300

    initial setup for caixabank microservice project

---
+++ antifraud-service/src/test/java/com/caixabank/antifraud_service/AntifraudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caixabank.antifraud_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntifraudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
